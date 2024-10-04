db413de18603c50ac312cd49569323f06170d0c4
Megidy <byrka.sergiy@gmail.com>
2024-10-04 21:18:58 +0300

    Added kitchen producer .

--- app/pkg/kitchen/consumer/main.go
+++ app/pkg/kitchen/consumer/main.go
@@ -1,5 +1,55 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
+	"github.com/IBM/sarama"
+)
+
+const (
+	Topic string = "orders"
+)
+
+func main() {
+	worker, err := ConnectConsumer([]string{"kafka:9092"})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	consumer, err := worker.ConsumePartition(Topic, 0, sarama.OffsetOldest)
+	if err != nil {
+		log.Fatal(err)
+	}
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	doneCh := make(chan struct{})
+	go func() {
+		select {
+		case err := <-consumer.Errors():
+			log.Println(err)
+		case msg := <-consumer.Messages():
+
+			order := string(msg.Value)
+			log.Println("received order from service orders ", order)
+		case <-sigChan:
+			log.Println("interrupt")
+			doneCh <- struct{}{}
+		}
+
+	}()
+
+	<-doneCh
+	err = worker.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+func ConnectConsumer(brokers []string) (sarama.Consumer, error) {
+	config := sarama.NewConfig()
+	config.Consumer.Return.Errors = true
+	return sarama.NewConsumer(brokers, config)
 }
