ba5fe0b7cf0a80c3aecf5fa31a5b6addd19b1843
Megidy <byrka.sergiy@gmail.com>
2024-10-04 18:13:32 +0300

    changed some functional , added orders

--- app/pkg/services/orders/producer.go
+++ app/pkg/services/orders/producer.go
@@ -1,1 +1,12 @@
 package orders
+
+import (
+	"github.com/Megidy/OrderManagmentSystem/pkg/types"
+	"github.com/gin-gonic/gin"
+)
+
+func HandleCreateOrder(c *gin.Context) {
+	var NewOrder types.Order
+
+	err := c.ShouldBindBodyWithJSON(&NewOrder)
+}
--- app/pkg/types/orders.go
+++ app/pkg/types/orders.go
@@ -1,1 +1,21 @@
 package types
+
+import "time"
+
+type OrderServices interface {
+	CreateOrder(Order) error
+	GetOrder(Order) (Order, error)
+	SendOrder(Order) error
+}
+
+type Dish struct {
+	Name     string `json:"name"`
+	Quantity int    `json:"quantity"`
+}
+
+type Order struct {
+	CustomersId int       `json:"id"`
+	Dishes      []Dish    `json:"dishes"`
+	Created     time.Time `json:"created"`
+	Status      string    `json:"status"`
+}
