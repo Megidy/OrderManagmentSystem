8ef666621a2c59a1a4393a3796e8c89a5b658c89
Megidy <byrka.sergiy@gmail.com>
2024-10-05 23:40:34 +0300

    Added CheckOrder and changed some code for CreateOrder.

--- app/pkg/orders/producer/producer.go
+++ app/pkg/orders/producer/producer.go
@@ -3,9 +3,11 @@ package producer
 import (
 	"encoding/json"
 	"log"
+	"math/rand"
 	"net/http"
 
 	"github.com/IBM/sarama"
+	"github.com/Megidy/OrderManagmentSystem/pkg/db"
 	"github.com/Megidy/OrderManagmentSystem/pkg/types"
 	"github.com/Megidy/OrderManagmentSystem/pkg/utils"
 	"github.com/gin-gonic/gin"
@@ -17,12 +19,23 @@ const (
 
 func HandleCreateOrder(c *gin.Context) {
 
-	var order types.Order
+	var NewOrder types.CreateOrder
 
-	err := c.ShouldBindJSON(&order)
+	err := c.ShouldBindJSON(&NewOrder)
 	if err != nil {
 		utils.HandleError(c, err, "failed to read body", http.StatusBadRequest, nil)
 		return
+	}
+
+	//TODO
+	//create more uniqu ids
+	orderId := rand.Intn(100)
+	customerId := rand.Intn(100)
+	order := types.Order{
+		OrderId:    orderId,
+		CustomerId: customerId,
+		Dishes:     NewOrder.Dishes,
+		Status:     "pending",
 	}
 
 	orderInBytes, err := json.Marshal(order)
@@ -30,13 +43,35 @@ func HandleCreateOrder(c *gin.Context) {
 		utils.HandleError(c, err, "failed to marshal body ", http.StatusInternalServerError, nil)
 		return
 	}
-	err = PushOrderToQueue(Topic, orderInBytes)
+
+	err = PushOrderToQueue(Topic, orderInBytes, "Create_order")
 	if err != nil {
 		utils.HandleError(c, err, "failed to push order ", http.StatusInternalServerError, nil)
 		return
 	}
+
+	err = db.CreateOrder(order)
+	if err != nil {
+		utils.HandleError(c, err, "failed to create order", http.StatusInternalServerError, nil)
+		return
+	}
+
 	c.JSON(http.StatusOK, gin.H{
 		"success": "order is handling ",
+	})
+
+}
+
+func HandleCheckOrders(c *gin.Context) {
+
+	checkOrders, err := db.CheckOrdersStatus()
+	if err != nil {
+		utils.HandleError(c, err, "failed to get CheckOrderStatus", http.StatusInternalServerError, nil)
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"orders ": checkOrders,
 	})
 }
 
@@ -50,7 +85,7 @@ func ConnectProducer(brokers []string) (sarama.SyncProducer, error) {
 
 }
 
-func PushOrderToQueue(topic string, message []byte) error {
+func PushOrderToQueue(topic string, message []byte, key string) error {
 	brokers := []string{"kafka:9092"}
 	producer, err := ConnectProducer(brokers)
 	if err != nil {
@@ -59,14 +94,14 @@ func PushOrderToQueue(topic string, message []byte) error {
 	defer producer.Close()
 	msg := sarama.ProducerMessage{
 		Topic: topic,
-		//add key later
+		Key:   sarama.StringEncoder(key),
 		Value: sarama.StringEncoder(message),
 	}
 	partition, offset, err := producer.SendMessage(&msg)
 	if err != nil {
 		return err
 	}
-	log.Printf("status is stored in topic(%s)/partition(%d)/offset(%d)\n",
-		topic, partition, offset)
+	log.Printf("status is stored in topic(%s)/partition(%d)/offset(%d),key(%s)\n",
+		topic, partition, offset, key)
 	return nil
 }
