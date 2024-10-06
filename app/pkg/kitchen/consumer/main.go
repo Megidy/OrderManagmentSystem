090c72de46db031ede28b583a6424f0f13e22cd0
Megidy <byrka.sergiy@gmail.com>
2024-10-06 17:21:38 +0300

    Changed error handling where occuring bugs.

--- app/pkg/kitchen/consumer/main.go
+++ app/pkg/kitchen/consumer/main.go
@@ -11,8 +11,8 @@ import (
 
 	"github.com/IBM/sarama"
 	"github.com/Megidy/OrderManagmentSystem/pkg/db"
+	"github.com/Megidy/OrderManagmentSystem/pkg/kitchen/producer"
 	"github.com/Megidy/OrderManagmentSystem/pkg/types"
-	"github.com/Megidy/OrderManagmentSystem/pkg/utils"
 	"github.com/goccy/go-json"
 )
 
@@ -39,7 +39,7 @@ func main() {
 		for {
 			select {
 			case err := <-consumer.Errors():
-				utils.HandleError(nil, err, "error when retrieveing data from producer ", 0, nil)
+
 				log.Println(err)
 			case msg := <-consumer.Messages():
 				key := string(msg.Key)
@@ -48,17 +48,14 @@ func main() {
 
 					err := json.Unmarshal(msg.Value, &order)
 					if err != nil {
-						utils.HandleError(nil, err, "failed to unmarshal data", 0, nil)
+
+						log.Println(err)
 					}
 					// order := string(msg.Value)
 					log.Println("received order from service orders ", order)
 					wg.Add(1)
 					fmt.Println(order)
 					go ProcessOrder(order, &wg)
-
-				}
-				if key == "Check_order" {
-					//another one
 
 				}
 
@@ -87,13 +84,15 @@ func ProcessOrder(order types.Order, wg *sync.WaitGroup) error {
 	}
 	for i := 0; i < 10; i++ {
 		time.Sleep(1000 * time.Millisecond)
-		log.Println("preparing dishes")
+		log.Println("preparing order", order.OrderId, " :", i*10, "%")
+
 	}
-	log.Println("dinner is ready!!")
+	log.Println("order :", order.OrderId, " is ready!!")
 	err = db.ChangeOrderStatus(order, "completed")
 	if err != nil {
 		return err
 	}
+	producer.SendKitchenMessage(order.OrderId, order.CustomerId)
 
 	return nil
 }
