a1143b805b5cbfadc14ee2a7596c538036beacea
Megidy <byrka.sergiy@gmail.com>
2024-10-06 17:20:17 +0300

    Added GetOrder ,DeleteOrder,DeleteDishes,GetDishes,CheckcustomersOrdersStatus functions

--- app/pkg/db/orders.go
+++ app/pkg/db/orders.go
@@ -15,7 +15,7 @@ func init() {
 }
 func CreateOrder(order types.Order) error {
 
-	_, err := db.Exec("INSERT INTO orders (customer_id, order_id, status) VALUES (?, ?, ?)",
+	_, err := db.Exec("insert into orders (customer_id, order_id, status) values (?, ?, ?)",
 		order.CustomerId, order.OrderId, order.Status)
 	if err != nil {
 		return err
@@ -31,15 +31,91 @@ func CreateOrder(order types.Order) error {
 	return nil
 }
 
+func GetOrder(orderId, customerId int) (types.Order, error) {
+	var order types.Order
+	row := db.QueryRow("select * from orders where order_id=?", orderId)
+	err := row.Scan(&order.OrderId, &order.CustomerId, &order.Created, &order.Status)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return types.Order{}, nil
+		}
+		return types.Order{}, err
+	}
+	return order, nil
+
+}
+func DeleteDishes(orderId int) error {
+	_, err := db.Exec("delete from dishes where order_id =?", orderId)
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
+func DeleteOrder(orderId int) error {
+	_, err := db.Exec("delete from orders where order_id =?", orderId)
+	if err != nil {
+		return err
+	}
+	err = DeleteDishes(orderId)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func GetDishes(orderId int) ([]types.Dish, error) {
+	var dishes []types.Dish
+	query, err := db.Query("select name,quantity from dishes where order_id=?", orderId)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, nil
+		}
+		return nil, err
+
+	}
+	for query.Next() {
+		var dish types.Dish
+		err = query.Scan(&dish.Name, &dish.Quantity)
+		if err != nil {
+			return nil, err
+		}
+		dishes = append(dishes, dish)
+	}
+	return dishes, nil
+}
+
 func CreateDish(orderId int, dish types.Dish) error {
 
-	_, err := db.Exec("INSERT INTO dishes (order_id, name, quantity) VALUES (?, ?, ?)",
+	_, err := db.Exec("insert into dishes (order_id, name, quantity) values (?, ?, ?)",
 		orderId, dish.Name, dish.Quantity)
 	if err != nil {
 		return err
 	}
 	return nil
 }
+
+func CheckcustomersOrdersStatus(orderId int) ([]types.CheckOrder, error) {
+	var Orders []types.CheckOrder
+	query, err := db.Query("select status,order_id from orders where order_id=?", orderId)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, nil
+		}
+		return nil, err
+	}
+	for query.Next() {
+		var checkOrder types.CheckOrder
+		err = query.Scan(&checkOrder.Status, &checkOrder.OrderId)
+		if err != nil {
+			return nil, err
+		}
+		Orders = append(Orders, checkOrder)
+	}
+
+	return Orders, nil
+}
+
 func CheckOrdersStatus() ([]types.CheckOrder, error) {
 	var Orders []types.CheckOrder
 	query, err := db.Query("select status,order_id from orders")
@@ -61,6 +137,7 @@ func CheckOrdersStatus() ([]types.CheckOrder, error) {
 	return Orders, nil
 
 }
+
 func ChangeOrderStatus(order types.Order, status string) error {
 	_, err := db.Exec("update orders set status =? where order_id=? and customer_id=?", status, order.OrderId, order.CustomerId)
 	if err != nil {
