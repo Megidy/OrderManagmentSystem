112ce462dc6dfa5ec626a99ec316b8ab4db7440f
Megidy <byrka.sergiy@gmail.com>
2024-10-06 17:17:32 +0300

    modified tables

--- tables.sql
+++ tables.sql
@@ -1,11 +1,11 @@
-CREATE TABLE IF NOT EXIST orders(
+CREATE TABLE orders(
     order_id INT PRIMARY KEY ,
     customer_id INT ,
     created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     status ENUM('pending', 'in_progress', 'completed', 'cancelled') NOT NULL
 )
 
-CREATE TABLE IF NOT EXISTS dishes(
+CREATE TABLE dishes(
     order_id INT  ,
     name VARCHAR(255),
     quantity INT
