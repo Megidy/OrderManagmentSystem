7d9557fc06a76b179acf646e36d9bcbbe773ed3f
Megidy <byrka.sergiy@gmail.com>
2024-10-06 17:26:46 +0300

    changed Order.created from time.time to string for correct response from db

--- app/pkg/types/orders.go
+++ app/pkg/types/orders.go
@@ -1,20 +1,16 @@
 package types
 
-import (
-	"time"
-)
-
 type Dish struct {
 	Name     string `json:"name"`
 	Quantity int    `json:"quantity"`
 }
 
 type Order struct {
-	OrderId    int       `json:"order_id"`
-	CustomerId int       `json:"customer_id"`
-	Dishes     []Dish    `json:"dishes"`
-	Created    time.Time `json:"created"`
-	Status     string    `json:"status"`
+	OrderId    int    `json:"order_id"`
+	CustomerId int    `json:"customer_id"`
+	Dishes     []Dish `json:"dishes"`
+	Created    string `json:"created"`
+	Status     string `json:"status"`
 }
 type CreateOrder struct {
 	Dishes []Dish `json:"dishes"`
